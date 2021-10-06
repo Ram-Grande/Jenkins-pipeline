74eb8e2060893067c2162a2828e7708b4feeedf9
Ram-Grande <91180158+Ram-Grande@users.noreply.github.com>
2021-10-06 14:30:46 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-pods=$(kubectl get pods -n ${params.Namespace} | grep ${params.Replicaset} | awk '{print $1}')
-m=0
-for pods in $pods
-do
-  ((m++)
-  kubectl delete pod $pod -n ${params.Namespace}
   if [[$m -eq $n]]; then
       break
   fi
